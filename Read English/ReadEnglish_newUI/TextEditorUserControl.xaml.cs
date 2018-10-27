81c6d0a2af689294d5b26bd80f469e3c8ef6fd38
Sedlyarskiy <ya.halo-halo@yandex.ru>
2018-10-27 18:28:55 +0400

    Начат новый UserControl - Текстовый редактор (TextEditorUserControl). Сделаны основные элементы редактирования текста.

---
+++ Read English/ReadEnglish_newUI/TextEditorUserControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadEnglish_newUI
+{
+    /// <summary>
+    /// Логика взаимодействия для textEditorUserControl.xaml
+    /// </summary>
+    public partial class textEditorUserControl : UserControl
+    {
+        public textEditorUserControl()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);            
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            cmbFontSize.Text = "14";
+            cmbFontFamily.Text = "Arial";
+        }
+
+        private void mainRTB_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (mainRTB.Selection.Text.Trim() != string.Empty)
+            {
+                object temp = mainRTB.Selection.GetPropertyValue(Inline.FontWeightProperty);
+                btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+                temp = mainRTB.Selection.GetPropertyValue(Inline.FontStyleProperty);
+                btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+                temp = mainRTB.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+                btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+                temp = mainRTB.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+                cmbFontFamily.SelectedItem = temp;
+                temp = mainRTB.Selection.GetPropertyValue(Inline.FontSizeProperty);
+                cmbFontSize.Text = temp.ToString();
+            }
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                mainRTB.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //mainRTB.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+        private void cmbFontSize_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (cmbFontSize.SelectedItem != null)
+                mainRTB.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.SelectedItem);
+        }
+    }
+}
